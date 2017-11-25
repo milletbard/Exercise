0f9f88c3d3b1a3b0e49895d519050a7332b3a19f
milletbard <milletbard@gmail.com>
2017-11-25 22:50:50 +0800

    update

--- todolist.js
+++ todolist.js
@@ -3,14 +3,19 @@ var app = new Vue({
 	el: '#taskApp',
 	data:{
 		Tasks:[
-		{name:'餵貓'}
+		{name: '減肥',Done: true}
 		]
 	},
 	methods: {
 		AddTask: function() {
-			this.Tasks.push({
-				name: this.Tasks.name
-			})
+			
+			if ( this.Tasks.name === null ) {
+				alert('null');
+			} else {
+				this.Tasks.push({
+					name: this.Tasks.name, done: false
+				}) 
+			}
 		}
 	}
 })
